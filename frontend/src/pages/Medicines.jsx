5c2ff47e5f23899052fe32a7fe218a67f6b4ffea
Dharmendra Behera <dharmendrabehera680@gmail.com>
2026-06-23 07:20:41 +0530

    Update Medicines.jsx

--- frontend/src/pages/Medicines.jsx
+++ frontend/src/pages/Medicines.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 import "../css/medicine.css";
 
 const Medicines = () => {
-  const BASE_URL = "http://localhost:5000";
+  const BASE_URL = "https://hospitalmanagementsystem-nz84.onrender.com";
 
   const user = JSON.parse(localStorage.getItem("user")) || {};
 
