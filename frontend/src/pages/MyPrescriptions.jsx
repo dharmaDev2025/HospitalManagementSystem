4ce41d46531c53797053f8a1d925eb9656a0df58
Dharmendra Behera <dharmendrabehera680@gmail.com>
2026-06-23 07:22:07 +0530

    Change BASE_URL to production endpoint

--- frontend/src/pages/MyPrescriptions.jsx
+++ frontend/src/pages/MyPrescriptions.jsx
@@ -5,7 +5,7 @@ import Navbar from "../components/Navbar";
 import "../css/myprescriptions.css";
 
 const MyPrescriptions = () => {
-  const BASE_URL = "http://localhost:5000";
+  const BASE_URL = "https://hospitalmanagementsystem-nz84.onrender.com";
 
   const user = JSON.parse(localStorage.getItem("user")) || {};
   const patientId = user._id || localStorage.getItem("userId");
