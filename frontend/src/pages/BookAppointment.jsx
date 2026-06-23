5cdca32daa22d3e753d05ad1c618f826caf80f43
Dharmendra Behera <dharmendrabehera680@gmail.com>
2026-06-23 07:14:21 +0530

    Update BASE_URL to production endpoint

--- frontend/src/pages/BookAppointment.jsx
+++ frontend/src/pages/BookAppointment.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 import "../css/appointment.css";
 
 const BookAppointment = () => {
-  const BASE_URL = "http://localhost:5000";
+  const BASE_URL = "https://hospitalmanagementsystem-nz84.onrender.com";
 
   const user =
     JSON.parse(localStorage.getItem("user")) || {};
